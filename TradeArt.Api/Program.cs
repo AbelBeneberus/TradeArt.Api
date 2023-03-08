4d8acc824e148f1d6dd084d8bd7c97d881c83052
Abel Beneberu <abel.beneberu@betssongroup.com>
2023-03-08 11:13:31 +0300

    enable swagger for any env

--- TradeArt.Api/Program.cs
+++ TradeArt.Api/Program.cs
@@ -36,12 +36,8 @@ try
 {
 	var app = builder.Build();
 
-
-	if (app.Environment.IsDevelopment())
-	{
-		app.UseSwagger();
-		app.UseSwaggerUI();
-	}
+	app.UseSwagger();
+	app.UseSwaggerUI();
 
 	app.UseHttpsRedirection();
 
